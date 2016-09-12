f6e6e912dd801179653c1175b56cc7a1601c6de5
fengshuo <fengshuo142003@gmail.com>
2016-09-11 20:47:34 -0400

    add comments

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Remarkable from 'remarkable';
 
 
@@ -6,11 +6,25 @@ import Remarkable from 'remarkable';
 class Comment extends Component {
   constructor(){
     super();
+    /*
+      0. When write React app with ES6, you'll need to set up the initial state property in the constructore with `this.state`
+      1. In a child class constructor, `this` cannot be used until `super` is called
+      2. When to pass `props` in constructor: when you need to access `this.props` in constructor
+      3. you can only use `super()` in a derived class, and you must call `super()` before accessing `this` in the constructor
+    */
     this.rawMarkup = this.rawMarkup.bind(this)
   }
   rawMarkup() {
     let md = new Remarkable();
     let rawMarkup = md.render(this.props.children.toString());
+    /*
+      1. Usually, a component's children is an array of components
+      But if there is only a single child, then this.props.children will be the single child itself without the array wrapper
+
+      2. You can't access children of the component via this.props.children.
+      In this example, you can't use this.props.children to get what's in <div className="comment"> (use refs instead:https://facebook.github.io/react/docs/more-about-refs.html)
+      this.props.children refers to the children between <Comment></Comment> (which is defined in <CommentList />)
+    */
     return {__html: rawMarkup}
   }
 
@@ -50,6 +64,12 @@ class CommentForm extends Component {
     this.handleAuthorChange = this.handleAuthorChange.bind(this);
     this.handleTextChange = this.handleTextChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    /*
+      In ES6 classes, there is no autobinding. You'll have to explicitly use either `bind(this)` or `arrow functions`
+      - bind this in constructor is recommended
+      - <div onClick={this.tick.bind(this)}>
+      - <div onClick={() => this.tick()}>
+    */
   }
 
   handleAuthorChange(e) {
@@ -79,7 +99,7 @@ class CommentForm extends Component {
         <input
           type="text"
           placeholder="your content"
-          valur={this.state.text}
+          value={this.state.text}
           onChange={this.handleTextChange}/>
         <input type="submit" value="Post Your Comment"/>
       </form>
@@ -129,5 +149,17 @@ class CommentBox extends Component {
     )
   }
 }
+
+CommentList.propTypes = {
+  data: PropTypes.array.isRequired
+}
+
+// propTypes and defaultProps are defined as properties on the constructor in ES6
+
+CommentForm.propTypes = {
+  onCommitSubmit: PropTypes.func.isRequired
+}
+
+
 
 export default CommentBox;
